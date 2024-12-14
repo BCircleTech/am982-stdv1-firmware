dae3800a10f9c7e450f50d4f25290100e884833a
Hang <bctech@foxmail.com>
2024-12-14 13:50:55 +0800

    rtkcom3 task

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -47,10 +47,17 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
+/* Definitions for Main */
+osThreadId_t MainHandle;
+const osThreadAttr_t Main_attributes = {
+  .name = "Main",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for RTKCOM1 */
+osThreadId_t RTKCOM1Handle;
+const osThreadAttr_t RTKCOM1_attributes = {
+  .name = "RTKCOM1",
   .stack_size = 512 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
@@ -60,7 +67,8 @@ const osThreadAttr_t defaultTask_attributes = {
 
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void *argument);
+void StartMain(void *argument);
+void StartRTKCOM1(void *argument);
 
 extern void MX_USB_DEVICE_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
@@ -92,8 +100,11 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  /* creation of Main */
+  MainHandle = osThreadNew(StartMain, NULL, &Main_attributes);
+
+  /* creation of RTKCOM1 */
+  RTKCOM1Handle = osThreadNew(StartRTKCOM1, NULL, &RTKCOM1_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -105,24 +116,42 @@ void MX_FREERTOS_Init(void) {
 
 }
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_StartMain */
 /**
-  * @brief  Function implementing the defaultTask thread.
+  * @brief  Function implementing the Main thread.
   * @param  argument: Not used
   * @retval None
   */
-/* USER CODE END Header_StartDefaultTask */
-__weak void StartDefaultTask(void *argument)
+/* USER CODE END Header_StartMain */
+__weak void StartMain(void *argument)
 {
   /* init code for USB_DEVICE */
   MX_USB_DEVICE_Init();
-  /* USER CODE BEGIN StartDefaultTask */
+  /* USER CODE BEGIN StartMain */
   /* Infinite loop */
   for(;;)
   {
     osDelay(1);
   }
-  /* USER CODE END StartDefaultTask */
+  /* USER CODE END StartMain */
+}
+
+/* USER CODE BEGIN Header_StartRTKCOM1 */
+/**
+* @brief Function implementing the RTKCOM1 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartRTKCOM1 */
+__weak void StartRTKCOM1(void *argument)
+{
+  /* USER CODE BEGIN StartRTKCOM1 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartRTKCOM1 */
 }
 
 /* Private application code --------------------------------------------------*/

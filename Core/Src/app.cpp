9db9d3e3456f57b1e55f420993b02d5fc5546b47
Hang <bctech@foxmail.com>
2024-12-26 00:29:53 +0800

    Update app

--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -6,15 +6,17 @@ extern "C"
 #include "message_buffer.h"
 #include "usbd_cdc_if.h"
 
+uint8_t initFlag = 0;
+
 uint8_t rtkCOM1RxBuff[512];
 uint8_t rtkCOM3RxBuff[512];
 
 // MessageBufferHandle_t mainToUSB;
 MessageBufferHandle_t usbToMain;
-// MessageBufferHandle_t mainToRTKCOM1;
+MessageBufferHandle_t mainToRTKCOM1;
 MessageBufferHandle_t rtkCOM1ToMain;
 MessageBufferHandle_t rtkCOM3ToMain;
-// MessageBufferHandle_t mainToIMU;
+MessageBufferHandle_t mainToIMU;
 // MessageBufferHandle_t imuToMain;
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size)
@@ -22,12 +24,18 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size)
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
     if (huart->Instance == rtkCOM1Ptr->Instance)
     {
-        xMessageBufferSendFromISR(rtkCOM1ToMain, rtkCOM1RxBuff, size, &xHigherPriorityTaskWoken);
+        if (initFlag)
+        {
+            xMessageBufferSendFromISR(rtkCOM1ToMain, rtkCOM1RxBuff, size, &xHigherPriorityTaskWoken);
+        }
         HAL_UARTEx_ReceiveToIdle_DMA(rtkCOM1Ptr, rtkCOM1RxBuff, sizeof(rtkCOM1RxBuff));
     }
     else if (huart->Instance == rtkCOM3Ptr->Instance)
     {
-        xMessageBufferSendFromISR(rtkCOM3ToMain, rtkCOM3RxBuff, size, &xHigherPriorityTaskWoken);
+        if (initFlag)
+        {
+            xMessageBufferSendFromISR(rtkCOM3ToMain, rtkCOM3RxBuff, size, &xHigherPriorityTaskWoken);
+        }
         HAL_UARTEx_ReceiveToIdle_DMA(rtkCOM3Ptr, rtkCOM3RxBuff, sizeof(rtkCOM3RxBuff));
     }
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
@@ -35,9 +43,12 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size)
 
 void USB_CDC_RxHandler(uint8_t *data, uint32_t size)
 {
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    xMessageBufferSendFromISR(usbToMain, data, size, &xHigherPriorityTaskWoken);
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    if (initFlag)
+    {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        xMessageBufferSendFromISR(usbToMain, data, size, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
 }
 
 void StartMain(void *argument)
@@ -49,10 +60,10 @@ void StartMain(void *argument)
 
     // mainToUSB = xMessageBufferCreate(1024);
     usbToMain = xMessageBufferCreate(1024);
-    // mainToRTKCOM1 = xMessageBufferCreate(1024);
+    mainToRTKCOM1 = xMessageBufferCreate(1024);
     rtkCOM1ToMain = xMessageBufferCreate(1024);
     rtkCOM3ToMain = xMessageBufferCreate(1024);
-    // mainToIMU = xMessageBufferCreate(1024);
+    mainToIMU = xMessageBufferCreate(1024);
     // imuToMain = xMessageBufferCreate(1024);
 
     ResetRTK();
@@ -61,6 +72,8 @@ void StartMain(void *argument)
     InitIMU(MPU6050_CLOCK_PLL_XGYRO, MPU6050_ACCEL_FS_2, MPU6050_GYRO_FS_250);
     SetIMUDigitalLowPassFilter(MPU6050_DLPF_BW_5);
     SetIMUSampleRate(100);
+
+    initFlag = 1;
 
     uint8_t usbRxBuffer[1024];
     size_t usbRxBufferLen;
@@ -95,6 +108,14 @@ void StartMain(void *argument)
 
         if (segment.GetOne(line))
         {
+            if (line[0] == 0x00)
+            {
+                xMessageBufferSend(mainToRTKCOM1, (const void *)line.c_str(), line.length(), 10);
+            }
+            else if (line[0] == 0x01)
+            {
+                xMessageBufferSend(mainToIMU, (const void *)line.c_str(), line.length(), 10);
+            }
         }
 
         osDelay(100);
@@ -103,8 +124,50 @@ void StartMain(void *argument)
 
 void StartRTKCOM1(void *argument)
 {
+    uint8_t mainRxBuffer[1024];
+    size_t mainRxBufferLen;
     while (1)
     {
+        if (initFlag)
+        {
+            mainRxBufferLen = xMessageBufferReceive(mainToRTKCOM1, mainRxBuffer, sizeof(mainRxBuffer), 10);
+            if (mainRxBufferLen > 0)
+            {
+                // parse mainRxBuffer
+                if (mainRxBuffer[1] == 0x00)
+                {
+                    SetRTKConf(mainRxBuffer + 2, mainRxBufferLen - 2);
+                }
+                else if (mainRxBuffer[1] == 0x01)
+                {
+                    if (mainRxBufferLen == 26)
+                    {
+                        double latitude, longitude, altitude;
+                        //
+                        SetRTKBaseWithPosition(latitude, longitude, altitude);
+                        //
+                    }
+                    else if (mainRxBufferLen == 3)
+                    {
+                        uint8_t seconds;
+                        seconds = mainRxBuffer[2];
+                        SetRTKBaseWithTime(seconds);
+                        //
+                    }
+                }
+                else if (mainRxBuffer[1] == 0x02)
+                {
+                    if (mainRxBufferLen == 3)
+                    {
+                        uint8_t freq;
+                        freq = mainRxBuffer[2];
+                        SetRTKRover(freq);
+                        //
+                    }
+                }
+            }
+        }
+
         osDelay(100);
     }
 }
@@ -113,14 +176,38 @@ void StartRTKCOM3(void *argument)
 {
     while (1)
     {
+        if (initFlag)
+        {
+        }
+
         osDelay(100);
     }
 }
 
 void StartIMU(void *argument)
 {
+    uint8_t mainRxBuffer[1024];
+    size_t mainRxBufferLen;
     while (1)
     {
+        if (initFlag)
+        {
+            mainRxBufferLen = xMessageBufferReceive(mainToIMU, mainRxBuffer, sizeof(mainRxBuffer), 10);
+            if (mainRxBufferLen > 0)
+            {
+                // parse mainRxBuffer
+                if (mainRxBuffer[1] == 0x00)
+                {
+                }
+                else if (mainRxBuffer[1] == 0x01)
+                {
+                }
+                else if (mainRxBuffer[1] == 0x02)
+                {
+                }
+            }
+        }
+
         osDelay(100);
     }
 }

6e5e3450b6f9eaae2eb258a4f0ca1f1a29fcd2af
Hang <bctech@foxmail.com>
2024-12-26 23:09:34 +0800

    Fix hw api bug

--- Core/Src/am982stdv1.c
+++ Core/Src/am982stdv1.c
@@ -45,69 +45,72 @@ void SetRTKConf(uint8_t *cmd, uint16_t size)
     tmp[size] = '\r';
     tmp[size + 1] = '\n';
     HAL_UART_Transmit_DMA(rtkCOM1Ptr, tmp, size + 2);
+    osDelay(10);
     free(tmp);
 }
 
 void SetRTKBaseWithPosition(double latitude, double longitude, double altitude)
 {
-    uint8_t cmd_0[] = "freset";
+    uint8_t cmd_0[] = "freset\r\n";
     uint8_t cmd_1[128] = {0};
-    uint8_t cmd_2[] = "rtcm1006 com2 10";
-    uint8_t cmd_3[] = "rtcm1033 com2 10";
-    uint8_t cmd_4[] = "rtcm1074 com2 1";
-    uint8_t cmd_5[] = "rtcm1124 com2 1";
-    uint8_t cmd_6[] = "rtcm1084 com2 1";
-    uint8_t cmd_7[] = "rtcm1094 com2 1";
-    uint8_t cmd_8[] = "saveconfig";
-    int cmd_1_len = snprintf((char *)cmd_1, sizeof(cmd_1), "mode base %.9f %.9f %.2f", latitude, longitude, altitude);
-    SetRTKConf(cmd_0, sizeof(cmd_0) - 1);
+    uint8_t cmd_2[] = "rtcm1006 com2 10\r\n";
+    uint8_t cmd_3[] = "rtcm1033 com2 10\r\n";
+    uint8_t cmd_4[] = "rtcm1074 com2 1\r\n";
+    uint8_t cmd_5[] = "rtcm1124 com2 1\r\n";
+    uint8_t cmd_6[] = "rtcm1084 com2 1\r\n";
+    uint8_t cmd_7[] = "rtcm1094 com2 1\r\n";
+    uint8_t cmd_8[] = "saveconfig\r\n";
+    int cmd_1_len = snprintf((char *)cmd_1, sizeof(cmd_1), "mode base %.9f %.9f %.2f\r\n", latitude, longitude, altitude);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_0, sizeof(cmd_0) - 1);
     osDelay(10000);
-    SetRTKConf(cmd_1, cmd_1_len);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_1, cmd_1_len);
     osDelay(100);
-    SetRTKConf(cmd_2, sizeof(cmd_2) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2, sizeof(cmd_2) - 1);
     osDelay(100);
-    SetRTKConf(cmd_3, sizeof(cmd_3) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3, sizeof(cmd_3) - 1);
     osDelay(100);
-    SetRTKConf(cmd_4, sizeof(cmd_4) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_4, sizeof(cmd_4) - 1);
     osDelay(100);
-    SetRTKConf(cmd_5, sizeof(cmd_5) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_5, sizeof(cmd_5) - 1);
     osDelay(100);
-    SetRTKConf(cmd_6, sizeof(cmd_6) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_6, sizeof(cmd_6) - 1);
     osDelay(100);
-    SetRTKConf(cmd_7, sizeof(cmd_7) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_7, sizeof(cmd_7) - 1);
     osDelay(100);
-    SetRTKConf(cmd_8, sizeof(cmd_8) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_8, sizeof(cmd_8) - 1);
+    osDelay(100);
 }
 
 void SetRTKBaseWithTime(unsigned int seconds)
 {
-    uint8_t cmd_0[] = "freset";
+    uint8_t cmd_0[] = "freset\r\n";
     uint8_t cmd_1[128] = {0};
-    uint8_t cmd_2[] = "rtcm1006 com2 10";
-    uint8_t cmd_3[] = "rtcm1033 com2 10";
-    uint8_t cmd_4[] = "rtcm1074 com2 1";
-    uint8_t cmd_5[] = "rtcm1124 com2 1";
-    uint8_t cmd_6[] = "rtcm1084 com2 1";
-    uint8_t cmd_7[] = "rtcm1094 com2 1";
-    uint8_t cmd_8[] = "saveconfig";
-    int cmd_1_len = snprintf((char *)cmd_1, sizeof(cmd_1), "mode base time %d", seconds);
-    SetRTKConf(cmd_0, sizeof(cmd_0) - 1);
+    uint8_t cmd_2[] = "rtcm1006 com2 10\r\n";
+    uint8_t cmd_3[] = "rtcm1033 com2 10\r\n";
+    uint8_t cmd_4[] = "rtcm1074 com2 1\r\n";
+    uint8_t cmd_5[] = "rtcm1124 com2 1\r\n";
+    uint8_t cmd_6[] = "rtcm1084 com2 1\r\n";
+    uint8_t cmd_7[] = "rtcm1094 com2 1\r\n";
+    uint8_t cmd_8[] = "saveconfig\r\n";
+    int cmd_1_len = snprintf((char *)cmd_1, sizeof(cmd_1), "mode base time %d\r\n", seconds);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_0, sizeof(cmd_0) - 1);
     osDelay(10000);
-    SetRTKConf(cmd_1, cmd_1_len);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_1, cmd_1_len);
     osDelay(100);
-    SetRTKConf(cmd_2, sizeof(cmd_2) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2, sizeof(cmd_2) - 1);
     osDelay(100);
-    SetRTKConf(cmd_3, sizeof(cmd_3) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3, sizeof(cmd_3) - 1);
     osDelay(100);
-    SetRTKConf(cmd_4, sizeof(cmd_4) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_4, sizeof(cmd_4) - 1);
     osDelay(100);
-    SetRTKConf(cmd_5, sizeof(cmd_5) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_5, sizeof(cmd_5) - 1);
     osDelay(100);
-    SetRTKConf(cmd_6, sizeof(cmd_6) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_6, sizeof(cmd_6) - 1);
     osDelay(100);
-    SetRTKConf(cmd_7, sizeof(cmd_7) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_7, sizeof(cmd_7) - 1);
     osDelay(100);
-    SetRTKConf(cmd_8, sizeof(cmd_8) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_8, sizeof(cmd_8) - 1);
+    osDelay(100);
 }
 
 /*
@@ -121,65 +124,66 @@ freq:
 */
 void SetRTKRover(unsigned int freq)
 {
-    uint8_t cmd_0[] = "freset";
-    uint8_t cmd_1[] = "mode rover";
-    uint8_t cmd_2_1hz[] = "gpgga com3 1";
-    uint8_t cmd_2_2hz[] = "gpgga com3 0.5";
-    uint8_t cmd_2_5hz[] = "gpgga com3 0.2";
-    uint8_t cmd_2_10hz[] = "gpgga com3 0.1";
-    uint8_t cmd_2_20hz[] = "gpgga com3 0.05";
-    uint8_t cmd_2_50hz[] = "gpgga com3 0.02";
-    uint8_t cmd_3_1hz[] = "gpths com3 1";
-    uint8_t cmd_3_2hz[] = "gpths com3 0.5";
-    uint8_t cmd_3_5hz[] = "gpths com3 0.2";
-    uint8_t cmd_3_10hz[] = "gpths com3 0.1";
-    uint8_t cmd_3_20hz[] = "gpths com3 0.05";
-    uint8_t cmd_3_50hz[] = "gpths com3 0.02";
-    uint8_t cmd_4[] = "saveconfig";
-    SetRTKConf(cmd_0, sizeof(cmd_0) - 1);
+    uint8_t cmd_0[] = "freset\r\n";
+    uint8_t cmd_1[] = "mode rover\r\n";
+    uint8_t cmd_2_1hz[] = "gpgga com3 1\r\n";
+    uint8_t cmd_2_2hz[] = "gpgga com3 0.5\r\n";
+    uint8_t cmd_2_5hz[] = "gpgga com3 0.2\r\n";
+    uint8_t cmd_2_10hz[] = "gpgga com3 0.1\r\n";
+    uint8_t cmd_2_20hz[] = "gpgga com3 0.05\r\n";
+    uint8_t cmd_2_50hz[] = "gpgga com3 0.02\r\n";
+    uint8_t cmd_3_1hz[] = "gpths com3 1\r\n";
+    uint8_t cmd_3_2hz[] = "gpths com3 0.5\r\n";
+    uint8_t cmd_3_5hz[] = "gpths com3 0.2\r\n";
+    uint8_t cmd_3_10hz[] = "gpths com3 0.1\r\n";
+    uint8_t cmd_3_20hz[] = "gpths com3 0.05\r\n";
+    uint8_t cmd_3_50hz[] = "gpths com3 0.02\r\n";
+    uint8_t cmd_4[] = "saveconfig\r\n";
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_0, sizeof(cmd_0) - 1);
     osDelay(10000);
-    SetRTKConf(cmd_1, sizeof(cmd_1) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_1, sizeof(cmd_1) - 1);
     osDelay(100);
     switch (freq)
     {
     case RTK_ROVER_FREQ_1HZ:
-        SetRTKConf(cmd_2_1hz, sizeof(cmd_2_1hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2_1hz, sizeof(cmd_2_1hz) - 1);
         osDelay(100);
-        SetRTKConf(cmd_3_1hz, sizeof(cmd_3_1hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3_1hz, sizeof(cmd_3_1hz) - 1);
         break;
     case RTK_ROVER_FREQ_2HZ:
-        SetRTKConf(cmd_2_2hz, sizeof(cmd_2_2hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2_2hz, sizeof(cmd_2_2hz) - 1);
         osDelay(100);
-        SetRTKConf(cmd_3_2hz, sizeof(cmd_3_2hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3_2hz, sizeof(cmd_3_2hz) - 1);
         break;
     case RTK_ROVER_FREQ_5HZ:
-        SetRTKConf(cmd_2_5hz, sizeof(cmd_2_5hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2_5hz, sizeof(cmd_2_5hz) - 1);
         osDelay(100);
-        SetRTKConf(cmd_3_5hz, sizeof(cmd_3_5hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3_5hz, sizeof(cmd_3_5hz) - 1);
         break;
     case RTK_ROVER_FREQ_10HZ:
-        SetRTKConf(cmd_2_10hz, sizeof(cmd_2_10hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2_10hz, sizeof(cmd_2_10hz) - 1);
         osDelay(100);
-        SetRTKConf(cmd_3_10hz, sizeof(cmd_3_10hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3_10hz, sizeof(cmd_3_10hz) - 1);
         break;
     case RTK_ROVER_FREQ_20HZ:
-        SetRTKConf(cmd_2_20hz, sizeof(cmd_2_20hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2_20hz, sizeof(cmd_2_20hz) - 1);
         osDelay(100);
-        SetRTKConf(cmd_3_20hz, sizeof(cmd_3_20hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3_20hz, sizeof(cmd_3_20hz) - 1);
         break;
     case RTK_ROVER_FREQ_50HZ:
-        SetRTKConf(cmd_2_50hz, sizeof(cmd_2_50hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2_50hz, sizeof(cmd_2_50hz) - 1);
         osDelay(100);
-        SetRTKConf(cmd_3_50hz, sizeof(cmd_3_50hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3_50hz, sizeof(cmd_3_50hz) - 1);
         break;
     default:
-        SetRTKConf(cmd_2_1hz, sizeof(cmd_2_1hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_2_1hz, sizeof(cmd_2_1hz) - 1);
         osDelay(100);
-        SetRTKConf(cmd_3_1hz, sizeof(cmd_3_1hz) - 1);
+        HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_3_1hz, sizeof(cmd_3_1hz) - 1);
         break;
     }
     osDelay(100);
-    SetRTKConf(cmd_4, sizeof(cmd_4) - 1);
+    HAL_UART_Transmit_DMA(rtkCOM1Ptr, cmd_4, sizeof(cmd_4) - 1);
+    osDelay(100);
 }
 
 void ResetIMU()
